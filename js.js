0694048d27b50ad18eee127bd35ca0dff5305e7b
Вячеслав Синяков <you@example.com>
2022-05-24 12:16:06 +0300

    rk_3

---
+++ js.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+const PORT = 5500;
+
+const express = require("express");
+   
+const app = express();
+   
+const urlencodedParser = express.urlencoded({extended: false});
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", urlencodedParser, function (request, response) {
+
+    if(!request.body) return response.sendStatus(400);
+   
+    console.log(request.body);
+    
+    let JSON = {
+       
+        diameter:request.body.d,
+        manufactor:request.body.manufactor
+    };
+    response.send(JSON);
+
+    
+});
+
+app.listen(5500, ()=>console.log("Сервер запущен..."));
+
